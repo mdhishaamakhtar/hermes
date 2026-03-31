ea0c6236643ca69fe3e26d25ca878448fcad2f02
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2026-03-31 18:54:25 +0530

    fixes
    Signed-off-by: Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>

--- backend/src/main/java/dev/hishaam/hermes/config/SecurityConfig.java
+++ backend/src/main/java/dev/hishaam/hermes/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
                         "/ws-hermes/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html",
-                        "/v3/api-docs/**")
+                        "/v3/api-docs/**",
+                        "/actuator/health")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
